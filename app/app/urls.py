42562dcac804150825f92b4bff3341599ba7d194
Raghav <raghav.ngfs@gmail.com>
2024-05-23 16:17:31 +0530

    Implemented filtering

--- app/app/urls.py
+++ app/app/urls.py
@@ -6,6 +6,10 @@ from drf_spectacular.views import (
     SpectacularAPIView,
     SpectacularSwaggerView,
 )
+
+from django.conf.urls.static import static
+from django.conf import settings
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/schema/', SpectacularAPIView.as_view(), name='api-schema'),
@@ -17,3 +21,10 @@ urlpatterns = [
     path('api/user/', include('user.urls')),
     path('api/recipe/', include('recipe.urls')),
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root = settings.MEDIA_ROOT
+    )
