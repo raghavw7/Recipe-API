02454c163c0aa9cfc7c0911c6bd3992bc5027c23
Raghav <raghav.ngfs@gmail.com>
2024-10-02 19:16:26 +0530

    idk

--- app/app/settings.py
+++ app/app/settings.py
@@ -133,9 +133,9 @@ STATIC_ROOT = '/vol/web/static'
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-USE_X_FORWARDED_HOST = True
-SECURE_SSL_REDIRECT = True
+# SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+# USE_X_FORWARDED_HOST = True
+# SECURE_SSL_REDIRECT = True
 
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
